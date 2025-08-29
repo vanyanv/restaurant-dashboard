992427bf35e2de027ca99f7c70e91ab80646f2b0
Vardan <vardan.vanyan@gmail.com>
2025-08-29 14:07:48 -0700

    made some adjustments for production

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,7 @@ const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
     '/': ['./prisma/**/*'],
   },
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs']
-  },
+  serverExternalPackages: ['@prisma/client', 'bcryptjs'],
   images: {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
